d0c0992afc254d54878570d8dce9d6085faed31b
gn-spawn <gandam00.transam@gmail.com>
2016-11-08 19:52:51 +0900

    inputviewcontrolleへのヒモ付

---
+++ Strike/InputViewController.swift
@@ -0,0 +1,39 @@
+//
+//  InputViewController.swift
+//  Strike
+//
+//  Created by Spawn on 2016/11/08.
+//  Copyright © 2016年 Spawn. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    @IBAction func titleField(_ sender: Any) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addTodo(_ sender: Any) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
