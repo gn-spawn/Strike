a076805b61a1292e0547107b694df87c7d7a6b0a
gn-spawn <gandam00.transam@gmail.com>
2016-11-09 10:28:37 +0900

    消せるよ

--- Strike/TodoTableViewController.swift
+++ Strike/TodoTableViewController.swift
@@ -75,17 +75,25 @@ class TodoTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            do {
+                let realm = try Realm()
+                try realm.write {
+                    realm.delete(self.todoItem[indexPath.row])
+                }
+            } catch {
+                
+            }
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+ 
 
     /*
     // Override to support rearranging the table view.
